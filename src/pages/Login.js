60a949534d96c882a90813aeb18b1c2ad2422dca
biboibob <bobbyardian.work@gmail.com>
2022-04-17 11:23:26 +0700

    CORS Policy

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,46 +1,96 @@
 import React, { useState } from "react";
 import Button from "@mui/material/Button";
+import { ToastContainer, toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import { PageRoutePath } from "../utils/config";
+import API from "../helper/api";
 
 //component
 import FormField from "../components/custom/FormField";
 
 function Login() {
+  const api = new API();
   let navigate = useNavigate();
 
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
+  const [form, setForm] = useState({
+    Username: "",
+    Password: "",
+  });
 
-  const onChangeForm = () => {};
+  const onChangeForm = (event) => {
+    const { name, value } = event.target;
+
+    const updatedForm = {
+      ...form,
+      [name]: value,
+    };
+
+    //console.log("Form Change", updatedForm);
+
+    setForm(updatedForm);
+  };
+
+  const onHandleRegister = () => {
+    const validateEmptyArray = Object.values(form).includes("");
+
+    if (validateEmptyArray) {
+      toast.info("There's Still Empty Field!");
+    } else {
+      onRegister();
+    }
+  };
+
+  const onRegister = () => {
+    const params = {
+      username: form.Username,
+      password: form.Password,
+    };
+  };
 
   const onNavigate = (val, data) => {
     navigate(val);
   };
 
   return (
-    <div className="flex flex-col m-auto w-3/12 shadow-2xl">
-      <h1 className="text-center text-2xl font-bold bg-primary-color text-white p-3">
-        Login Page
-      </h1>
-      <div className="flex flex-col px-10 py-6 gap-5">
-        <FormField label={"Username"} value={userName} vertical={true} />
+    <>
+      <ToastContainer />
+      <div className="flex flex-col m-auto w-3/12 shadow-2xl">
+        <h1 className="text-center text-2xl font-bold bg-primary-color text-white p-3">
+          Login Page
+        </h1>
+        <div className="flex flex-col px-10 py-6 gap-5">
+          <FormField
+            label={"Username"}
+            name={"Username"}
+            value={form.UserName}
+            onChange={onChangeForm}
+            vertical={true}
+          />
 
-        <FormField label={"Password"} value={userName} vertical={true} />
+          <FormField
+            label={"Password"}
+            name={"Password"}
+            value={form.Password}
+            onChange={onChangeForm}
+            vertical={true}
+          />
 
-        <Button variant="contained">Login</Button>
+          <Button variant="contained" onClick={() => onHandleRegister()}>
+            Login
+          </Button>
 
-        <span className="text-sm text-center mt-5">
-          Don't have an account yet?{" "}
-          <span
-            onClick={() => onNavigate(PageRoutePath.REGISTER)}
-            className="font-bold cursor-pointer hover:text-primary-color"
-          >
-            Create Account
+          <span className="text-sm text-center mt-5">
+            Don't have an account yet?{" "}
+            <span
+              onClick={() => onNavigate(PageRoutePath.REGISTER)}
+              className="font-bold cursor-pointer hover:text-primary-color"
+            >
+              Create Account
+            </span>
           </span>
-        </span>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
