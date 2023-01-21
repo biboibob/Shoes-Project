7fb8c34d8d82eb40b3e446e828d91ce8922fc780
biboibob <bobbyardian.work@gmail.com>
2023-01-22 01:16:53 +0700

    Adding Feature Sort

--- src/pages/DetailProduct.js
+++ src/pages/DetailProduct.js
@@ -219,7 +219,7 @@ function DetailProduct() {
   };
 
   return (
-    <div className="flex flex-col md:flex-row container min-h-full">
+    <div className="flex flex-col md:flex-row container min-h-full py-4">
       <div className="basis-1/2 px-[2.5rem] py-2 md:py-[3.125rem] flex justify-center md:sticky top-0">
         <ShoesPreview asset={ShoesFeatured} />
       </div>
@@ -251,7 +251,7 @@ function DetailProduct() {
               </div>
               <Button
                 className={
-                  "w-fit text-xs md:text-sm p-1 md:!p-2 hidden md:flex mt-2"
+                  "!w-fit !text-sm p-2 hidden md:flex mt-2"
                 }
                 value={`${readMore ? "Read More" : "Read Less"}`}
                 onClick={onHandleToggleDescription}
@@ -300,7 +300,7 @@ function DetailProduct() {
             </div>
 
             <Button
-              className={"w-fit text-xs p-1.5 flex md:hidden"}
+              className={"!w-fit !text-xs !p-2 flex md:hidden"}
               value={`${readMore ? "Read More" : "Read Less"}`}
               onClick={onHandleToggleDescription}
             />
